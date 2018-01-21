0905d4472814095e9d54ab8c8ab981d550e711b3
younux <benhoumich.younes@gmail.com>
2018-01-21 17:59:20 +0100

    Init the C++ project and add SFML

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# tic-tac-toe
+# Tic-Tac-Toe Game
+
+This is a Tic-Tac-Toe game developen 
+using C++ and SFML library.
+
+Developed on mac os x 10.13.2
---
+++ cmake-build-debug/src/CMakeFiles/tic_tac_toe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tic_tac_toe.dir/main.cpp.o"
+  "tic_tac_toe.pdb"
+  "tic_tac_toe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tic_tac_toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
