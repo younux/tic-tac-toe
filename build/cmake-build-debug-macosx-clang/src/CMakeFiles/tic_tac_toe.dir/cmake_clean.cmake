0be31d34e992c328962fc69a84b991a0eeeef76d
younux <benhoumich.younes@gmail.com>
2018-01-27 10:29:15 +0100

    Add build for macosx

---
+++ build/cmake-build-debug-macosx-clang/src/CMakeFiles/tic_tac_toe.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tic_tac_toe.dir/main.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/StateMachinne.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/AssetManager.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/InputManager.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/Game.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/SplashState.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/MainMenuState.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/GameState.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/PauseState.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/GameOverState.cpp.o"
+  "CMakeFiles/tic_tac_toe.dir/AI.cpp.o"
+  "tic_tac_toe.pdb"
+  "tic_tac_toe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tic_tac_toe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
